fef0ba7ca1614c37632956757420db15731af774
unknown <guerreiramaya2016@gmail.com>
2020-06-04 19:25:08 -0300

    frontend

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import './App.css';
+import Routes from './routes';
+
+
+function App() {
+
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
